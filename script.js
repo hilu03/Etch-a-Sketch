301683a87693e2defbcb43cebfc2f97f7e3d93a9
Luong Tran <tranthihienluong2003@gmail.com>
2024-07-16 22:42:42 +0700

    Create 16x16 grid

---
+++ script.js
@@ -0,0 +1,18 @@
+function renderGrid() {
+  const container = document.querySelector(".container");
+  let html = "";
+  for (let i=0; i<16; i++) {
+    html += `
+      <div class="row">
+    `;
+    for (let j=0; j<16; j++) {
+      html += `
+        <div class="cell"></div>
+    `;
+    }
+    html += "</div>";
+  }
+  container.innerHTML = html;
+}
+
+renderGrid();
