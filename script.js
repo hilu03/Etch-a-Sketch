70b0e801735481788ee0243a9ca5e608937d2396
Luong Tran <tranthihienluong2003@gmail.com>
2024-07-17 11:41:47 +0700

    Add input button and create click event for it

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-function renderGrid() {
+function renderGrid(size = 16) {
   const container = document.querySelector(".container");
   let html = "";
-  for (let i=0; i<16; i++) {
+  for (let i=0; i<size; i++) {
     html += `
       <div class="row">
     `;
-    for (let j=0; j<16; j++) {
+    for (let j=0; j<size; j++) {
       html += `
         <div class="cell"></div>
     `;
@@ -13,12 +13,39 @@ function renderGrid() {
     html += "</div>";
   }
   container.innerHTML = html;
+  setSizeOfCell(size);
+  addHoverEffect();
 }
 
-renderGrid();
-const cells = document.querySelectorAll(".cell");
-cells.forEach(cell => {
-  cell.addEventListener("mouseenter", () => {
-    cell.style.backgroundColor = "gray";
-  });
+function setSizeOfCell(size) {
+  const cells = document.querySelectorAll(".cell");
+  const width = document.querySelector(".row").offsetWidth;
+  cells.forEach(cell => {
+    cell.style.width = `${width / size}px`;
+    cell.style.height = `${width / size}px`;
 });
+}
+
+function addHoverEffect() {
+  const cells = document.querySelectorAll(".cell");
+  cells.forEach(cell => {
+    cell.addEventListener("mouseenter", () => {
+      cell.style.backgroundColor = "gray";
+    });
+  });
+}
+
+const inputButton = document.querySelector(".input-button");
+inputButton.addEventListener("click", () => {
+  let size = prompt("Please input number of cell per row.\n(Note: Maximum number is 100!)", 16);
+  if (size != null) {
+    if (size > 100) {
+      alert("Maximum number is 100! Try again!");
+    }
+    else {
+      renderGrid(Number(size));
+    }
+  }
+});
+
+renderGrid();
